9822c08e2de42c13757e7d74c9dcb3339e09de0e
AnonymousPC <shyeyian@petalmail.com>
2025-03-06 23:33:14 +0800

    sync my excellent work

---
+++ generic/io/mysql/mysql_base/mysql_stream.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+template < class type >
+concept mysql_mode = requires { typename type::mysql_mode_tag; };
+
+class mysql_stream
+{
+    public: // Typedef
+        using value_type =
+            variant<int64_t,
+                    uint64_t,
+                    float32_t,
+                    float64_t,
+                    string,
+                    vector<char>,
+                    time_point,
+                    duration,
+                    std::nullptr_t
+                   >;
+
+    public: // Core
+        mysql_stream ( ) = default;
+        mysql_stream ( url, mysql_mode auto... );
+
+    public: // Interface
+        mysql_stream&      open    ( url, mysql_mode auto... );
+        mysql_stream&      close   ( );
+        matrix<value_type> execute ( string, auto... );
+
+    public: // Modes
+        struct username;
+        struct password;
+        struct database;
+
+    private: // Data
+        boost::mysql::any_connection                  handle          = boost::mysql::any_connection(io_context);
+        unordered_map<string,boost::mysql::statement> server_stmtpool = {};
+        unordered_set<string>                         client_stmtpool = {};
+
+    private: // Detail
+        matrix<value_type>        execute_client_stmt ( string, auto... );
+        matrix<value_type>        execute_server_stmt ( string, auto... );
+        static matrix<value_type> execute_result      ( const boost::mysql::results& );
+        string                    format_client_stmt  ( const boost::mysql::error_with_diagnostics&, const string&, const auto&... ) const;
+        string                    format_server_stmt  ( const boost::mysql::error_with_diagnostics&, const string&, const auto&... ) const;
+        static auto               make_stmt_arg       ( const auto& );
+        static value_type         make_result_arg     ( const boost::mysql::field_view& );
+
+        struct empty_arg { };
+        friend class boost::mysql::formatter<empty_arg>;
+};
+
+#include "mysql_stream.ipp"
+#if dll
+    #include "mysql_stream.cpp"
+#endif
---
+++ generic/io/utility/exception/mysql_error_with_diagnostics.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace detail
+{
+    class mysql_error_with_diagnostics
+        extends public boost::mysql::error_with_diagnostics
+    {
+        public: // Core
+            mysql_error_with_diagnostics ( const boost::mysql::error_with_diagnostics& );
+
+        public: // What
+            virtual const char* what ( ) const noexcept;
+
+        private: // Data
+            string error_what = "";
+    };
+} // namespace detail
+
+#if dll
+    #include "mysql_error_with_diagnostics.cpp"
+#endif

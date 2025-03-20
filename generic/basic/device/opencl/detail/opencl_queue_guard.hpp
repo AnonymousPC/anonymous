cf932b7a696bb00c5ce67ca12dbbf3008484e1fa
AnonymousPC <shyeyian@petalmail.com>
2025-03-20 12:05:58 +0800

    ioexec

---
+++ generic/basic/device/opencl/detail/opencl_queue_guard.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace detail
+{
+    struct opencl_queue_guard
+    {
+        ~opencl_queue_guard ( );
+        static boost::compute::command_queue& command_queue ( );
+    };
+} // namespace detail
+
+#if dll
+    #include "opencl_queue_guard.cpp"
+#endif
